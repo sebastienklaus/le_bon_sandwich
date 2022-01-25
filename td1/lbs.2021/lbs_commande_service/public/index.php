fe286dd3ea12e4114b23d083b0a10ca04fe4074e
Sebastien Klaus <sebastienklaus7@gmail.com>
2022-01-25 17:50:50 +0100

    add td3 methods

--- td1/lbs.2021/lbs_commande_service/public/index.php
+++ td1/lbs.2021/lbs_commande_service/public/index.php
@@ -5,6 +5,7 @@ require_once  __DIR__ . '/../src/vendor/autoload.php';
 use \Psr\Http\Message\ServerRequestInterface as Request ;
 use \Psr\Http\Message\ResponseInterface as Response ;
 use \lbs\command\app\controller\TD1CommandController as TD1CommandController;
+use \lbs\command\app\controller\TD3CommandController as TD3CommandController;
 
 
 use \lbs\command\app\model\Commande as Commande;
@@ -23,8 +24,13 @@ $capsule->addConnection($app_config['settings']['dbfile']);
 $capsule->bootEloquent();
 $capsule->setAsGlobal();
 
-
+// TD1
 $app->get('/TD1/commands[/]', TD1CommandController::class . ':listCommands');
 $app->get('/TD1/commands/{id}[/]', TD1CommandController::class . ':oneCommand');
+
+
+//TD3
+$app->get('/TD3/commands[/]', TD3CommandController::class . ':form');
+$app->put('/TD3/commands/{id}[/]', TD3CommandController::class . ':updateCommand');
 
 $app->run();
