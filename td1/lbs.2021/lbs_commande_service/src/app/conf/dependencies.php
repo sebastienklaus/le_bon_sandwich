d5d26a71d96e6243cf211fe6fefabbb2e3357595
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-01 22:28:20 +0100

    configure logger

--- td1/lbs.2021/lbs_commande_service/src/app/conf/dependencies.php
+++ td1/lbs.2021/lbs_commande_service/src/app/conf/dependencies.php
@@ -2,12 +2,10 @@
 
 return[
 
-    $c['db'] = function ($c) {
-        
-    
-        $capsule = new \Illuminate\Database\Capsule\Manager;
-        $capsule->addConnection($this['settings']['dbfile']);
-        $capsule->bootEloquent();
-        $capsule->setAsGlobal();
-    }
+    'logger' => function(\Slim\Container $c ) {
+        $log= new \Monolog\Logger($c->settings['log.name']) ;
+        $log->pushHandler( new \Monolog\Handler\StreamHandler($c->settings['debug.log'],$c->settings['log.level']) ) ;
+        return $log ;
+    },
+
 ];
