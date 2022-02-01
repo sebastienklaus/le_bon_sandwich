307277e68e351bc23641cc0a8a6f9f8c5eed0405
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-01 15:43:26 +0100

    add item & paiement models

---
+++ td1/lbs.2021/lbs_commande_service/src/app/model/Paiement.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lbs\command\app\model;
+
+use \Psr\Http\Message\ServerRequestInterface as Request ;
+use \Psr\Http\Message\ResponseInterface as Response ;
+use Psr\Container\ContainerInterface;
+
+class Paiement extends \Illuminate\Database\Eloquent\Model {
+
+    protected $table      = 'paiement';  /* le nom de la table */
+
+
+    public  $incrementing = false;
+
+}
