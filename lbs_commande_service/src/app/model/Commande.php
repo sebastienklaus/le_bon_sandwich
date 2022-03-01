277fa5923f6ad1094650a5df82bd0166a91b1d71
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-01 18:02:28 +0100

    add timestamp

--- lbs_commande_service/src/app/model/Commande.php
+++ lbs_commande_service/src/app/model/Commande.php
@@ -13,8 +13,12 @@ class Commande extends \Illuminate\Database\Eloquent\Model {
 
     protected $table      = 'commande';  /* le nom de la table */
     protected $primaryKey = 'id';
+    public    $timestamps = true;    /* si vrai la table doit contenir
+                                        les deux colonnes updated_at,
+                                        created_at */
+    
 
-    public  $incrementing = false;      //pour primarykey, on annule l'auto_increment
+    public $incrementing = false;      //pour primarykey, on annule l'auto_increment
     public $keyType='string';
 
     public function items()
