63484d56db4eb4165846292d588a9540e9e904ae
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-03 19:45:14 +0100

    add comments + validation library

--- lbs_commande_service/src/app/middleware/Middleware.php
+++ lbs_commande_service/src/app/middleware/Middleware.php
@@ -9,9 +9,11 @@ use lbs\command\app\error\JsonError as JsonError;
 use Illuminate\Database\Eloquent\ModelNotFoundException as ModelNotFoundException;
 
 use Ramsey\Uuid\Uuid;
+use \DavidePastore\Slim\Validation\Validation as Validation ;
 
 class Middleware {
 
+    // * création d'un ID avec un UUID (utilisation de la librairie Ramsey\UUID)
     public static function createID(Request $req, Response $resp, callable $next){
         // création d'un ID avec la librairie UUID
         $uuid4_id = Uuid::uuid4();
@@ -22,7 +24,8 @@ class Middleware {
 
         return $resp;
     }
-        
+    
+    // * création d'un token avec random_bytes()
     public static function createToken(Request $req, Response $resp, callable $next){
         $token = random_bytes(32);
         $token = bin2hex($token);
