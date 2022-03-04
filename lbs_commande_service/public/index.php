73f9d638164e0668e8f4acfb0d6e3b1da70e9726
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-04 12:18:05 +0100

    add validators

--- lbs_commande_service/public/index.php
+++ lbs_commande_service/public/index.php
@@ -2,13 +2,11 @@
 
 require_once  __DIR__ . '/../src/vendor/autoload.php';
 
-use \Psr\Http\Message\ServerRequestInterface as Request ;
-use \Psr\Http\Message\ResponseInterface as Response ;
-
 use \lbs\command\app\controller\CommandController as CommandController;
 use \lbs\command\app\middleware\Middleware as Middleware;
 
-use \lbs\command\app\model\Commande as Commande;
+use \Respect\Validation\Validator as v;
+use \DavidePastore\Slim\Validation\Validation as Validation ;
 
 
 $settings = require_once __DIR__. '/../src/app/conf/settings.php';
@@ -25,6 +23,16 @@ $capsule->addConnection($app_config['settings']['dbfile']);
 $capsule->bootEloquent();
 $capsule->setAsGlobal();
 
+$validators = [
+    'nom' => v::StringType()->alpha() ,
+    // 'login' => v::alnum()->length(3,8) ,
+    'mail' => v::email() ,
+    'livraison' => [
+        'date' => v::date('d-m-Y')->min('now'),
+        'heure' => v::date('H:i')->between('00:00', '23:59'),
+    ],
+    ];
+
 
 
 $app->get('/commands[/]', CommandController::class . ':listCommands')
@@ -37,7 +45,8 @@ $app->get('/commands/{id}[/]', CommandController::class . ':oneCommand')
 $app->post('/commands[/]', CommandController::class . ':createCommand')
     ->setName('creationCommand')
     ->add(Middleware::class . ':createID')
-    ->add(Middleware::class . ':createToken');
+    ->add(Middleware::class . ':createToken')
+    ->add(new Validation( $validators) );
 
 $app->put('/commands/{id}[/]', CommandController::class . ':replaceCommand')->setName('replaceCommand');
     
