f092799a176f08c55a59063e930bd548de7f96e2
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-08 15:21:01 +0100

    add oneCommand

--- lbs_fabrication_service/src/app/controller/CommandController.php
+++ lbs_fabrication_service/src/app/controller/CommandController.php
@@ -22,16 +22,48 @@ class CommandController{
     {
         $this->container = $container;
     }
+    
     public function allCommands(Request $req, Response $resp, array $args): Response{
+        //get attribute 'status' from middleware 'filterStatus()'
+        $param_status = $req->getAttribute('status');
+
         //get all the commands
-        $commandes = Commande::select(['id', 'nom', 'mail', 'montant', 'livraison'])
-            ->get();
+        $allCommandes = Commande::select(['id', 'nom', 'created_at', 'livraison', 'status']);
+        //check for status filter
+        if($param_status){
+            $allCommandes = $allCommandes->where('status', '=', $param_status);
+        }
+        $allCommandes = $allCommandes->get();
+
+
+        // initaite array for datas + links
+        $command_and_links = [];
+
+
+        foreach($allCommandes as $commande){
+            // get the uri path for each command
+            $url_oneCommand = $this->container->router->pathFor('command', ['id'=>$commande['id']]);
+            // add to another array the details of each command (data + links)
+            $command_and_links[] = [
+                'command' => [
+                    'ds'=> $param_status,
+                    'id' => $commande['id'],
+                    'nom' => $commande['nom'],
+                    'created_at' => $commande['created_at'],
+                    'livraison' => $commande['livraison'],
+                    'status' => $commande['status']
+                ],
+                'links' => [
+                    'self' => [ 'href' => $url_oneCommand]
+                ]
+            ];
+        };
 
         //complete the data array with datas who are gonna be returned in JSON format
         $data = [
             "type" => "collection",
-            "count" => count($commandes),
-            "commandes" => $commandes,
+            "count" => count($allCommandes),
+            "commandes" => $command_and_links,
         ];
 
         //configure the response headers
@@ -45,6 +77,62 @@ class CommandController{
 
         //return the response (ALWAYS !)
         return $resp;
+    }
+
+
+    public function oneCommand(Request $req, Response $resp, array $args): Response {
+        //get the id in the URI with the args array
+        $id = $args['id'];
+        
+        //try & ctach in case the id doesn't exist
+        try {
+
+            //get the actual URI & params
+            $url_oneCommand = $this->container->router->pathFor('command', ['id'=>$id]);
+            // $url_itemsOfCommand = $this->container->router->pathFor('commandWithItems', ['id'=>$id]);
+            $param_embed = $req->getQueryParam('embed' , null); 
+
+            $param_token = $req->getAttribute('token');
+
+            //get the command with some id
+            $commande = Commande::select(['id', 'nom', 'created_at', 'livraison', 'mail', 'montant', 'livraison', 'token'])
+                ->where('id', '=', $id);
+            if($param_embed === 'items'){
+                $commande = $commande->with('items:id,libelle,tarif,quantite,command_id');
+            }    
+            $commande = $commande->firstOrFail();
+
+
+            //complete the data array with datas who are gonna be returned in JSON format
+            $data = [
+                "type" => "resource",
+                "commande" => $commande,
+                "links" => [
+                    // "items" => ["href" => $url_itemsOfCommand ],
+                    "self" => ["href" => "$url_oneCommand" ]
+                ]
+            ];
+            
+
+            //configure the response headers
+            $resp = $resp->withStatus(200)
+                ->withHeader('Content-Type', 'application/json; charset=utf-8');
+            
+            //write in the body with data encode with a json_encode function
+            $resp->getBody()->write(json_encode($data));
+                
+            
+            //return the response (ALWAYS !)
+            return $resp;
+
+        }
+        //in case there is 0 ressource with this id ... 
+        catch (ModelNotFoundException $e) {
+            $this->container->logger_debug->debug('GET / : debug (c\'est pas très grave, pas de gros soucis pour l\'instant');
+            $this->container->logger_warning->warning('GET / : warning (au secours, tout va mal chef !');
+            return JsonError::jsonError($req, $resp, 'error', 404,'Ressource not found : command ID = ' . $id );   
+
+        }
     }
 
 
